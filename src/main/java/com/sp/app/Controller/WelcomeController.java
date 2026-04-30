0fed6e5ac36f8e05f47add0d7253d6bbee6af866
Saurabh <saurabhpadalkar9758@gmail.com>
2026-04-30 20:27:06 +0530

    adding new methods & Annotations

--- src/main/java/com/sp/app/Controller/WelcomeController.java
+++ src/main/java/com/sp/app/Controller/WelcomeController.java
@@ -1,10 +1,16 @@
 package com.sp.app.Controller;
 
-
 import java.util.Arrays;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sp.app.Model.Student;
@@ -12,7 +18,7 @@ import com.sp.app.Model.Student;
 @RestController
 public class WelcomeController {
 	
-	@RequestMapping(value = "/Welcome")
+	@GetMapping(value = "/Welcome")
 	public String getWelcomeMsg() {
 		System.out.println("In Controler Layer");
 		String msg = "Welcome to Spring Boot REST-API";
@@ -20,16 +26,14 @@ public class WelcomeController {
 	}
 	
 	
-	
-	
-	@RequestMapping(value = "/Student")
+	@GetMapping(value = "/student")
 	public Student getStudent(){
 		Student stu = new Student(10, "saurabh", "kolhapur", 85.20f);
 		return stu;
 	}
 	
 	
-	@RequestMapping(value = "/Students")
+	@GetMapping(value = "/students")
 	public List<Student> getStudents(){
 		
 		Student stu1 = new Student(10, "saurabh", "kolhapur", 85.20f);
@@ -42,5 +46,37 @@ public class WelcomeController {
 		return list;
 	}
 	
+	
+	@PostMapping(value = "/student")
+	public String addStudent(@RequestBody Student student)
+	{
+		System.out.println("Student RollNo :" + student.getRollno());
+		System.out.println("Student Name :" + student.getName());
+		System.out.println("Student Address :" + student.getAddress());
+		System.out.println("Student Marks :" + student.getMarks());
+		
+		return "Records Added Successfully";	
+	}
+	
+	
+	// Path Parameter
+	
+	@GetMapping(value = "/checkEvenOdd/{num}")
+	public String checkNumber(@PathVariable int num)
+	{
+		String msg = (num % 2 == 0) ? "Even Number: " + num : "Odd Number: " + num;
+		return msg;	
+	}
+	
+	
+	// Query Parameter
+	
+	@GetMapping(value = "/login")
+	public String checkLogin(@RequestParam String userName, @RequestParam String password)
+	{
+	   System.out.println("User Name :" + userName);
+	   System.out.println("Password :" + password);
+	   return "Login Successfully";
+	}
 	
 }
